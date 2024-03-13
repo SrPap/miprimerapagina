919127e5027dc9d9c7a8e724c0535da1e8537e85
SrPap <163212935+SrPap@users.noreply.github.com>
2024-03-12 19:57:13 -0500

    reorganizing 1

--- mi_primera_web/navbar/script.js
+++
@@ -1,7 +0,0 @@
-
-
-let menu_responsive = document.querySelector(".checkbtn");
-menu_responsive.onclick = function () {
-  navBar = document.querySelector(".navbar");
-  navBar.classList.toggle("active");
-};
